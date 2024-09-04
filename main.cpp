cc2ca5f0ead59669aa7096e72dc0f4b1edc095aa
Jakub Schwenkbeck <Jakub@eSchwenkbeck>
2024-09-05 01:19:38 +0200

    CLI for user interaction of tools

--- main.cpp
+++ main.cpp
@@ -4,24 +4,33 @@
 
 int main(){
 
+
+    // Choice of Tool:
     int choice;
     std::cout << "Choose a tool:\n";
     std::cout << "1. Brute Force\n";
     std::cin >> choice;
 
 
- if (choice == 1) {
-        std::cout << "Enter Hash to crack: \n" ;
+    // if cases for choice:
+ if (choice == 1) {  // Brute force password cracking
+
+
+        std::cout << "Enter Hash to crack: \n" ; //Get hash to crack param
         std::string toCrack;
         std::cin >> toCrack;
-        std::cout << "Entered: "<< toCrack << " \n \n Please enter max length: \n";
+
+
+        std::cout << "Entered: "<< toCrack << " \n \n Please enter max length: \n"; // get Max Pass Length to crack
         int Max;
         std::cin >> Max;
-        std::cout << "Bruteforce started!" << std::endl;
+
+
+        std::cout << "Bruteforce started!" << std::endl; // start cracking hash
         Crack(toCrack,Max);
 
     } else {
-        std::cout << "Invalid choice!" << std::endl;
+        std::cout << "Invalid choice!" << std::endl; // default else
     }
 
    
