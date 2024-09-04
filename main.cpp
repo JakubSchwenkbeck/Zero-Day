a15564c954875de69b03f15dc50cc211079173cb
Jakub Schwenkbeck <Jakub@eSchwenkbeck>
2024-09-05 01:17:15 +0200

    Implemented simple CLI

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include "password_cracker/brute_force.h"
-
+#include <string>
 #include <iostream>
 
 int main(){
-    Crack("aaa",3);
+
+    int choice;
+    std::cout << "Choose a tool:\n";
+    std::cout << "1. Brute Force\n";
+    std::cin >> choice;
+
+
+ if (choice == 1) {
+        std::cout << "Enter Hash to crack: \n" ;
+        std::string toCrack;
+        std::cin >> toCrack;
+        std::cout << "Entered: "<< toCrack << " \n \n Please enter max length: \n";
+        int Max;
+        std::cin >> Max;
+        std::cout << "Bruteforce started!" << std::endl;
+        Crack(toCrack,Max);
+
+    } else {
+        std::cout << "Invalid choice!" << std::endl;
+    }
+
+   
     return 0;
 
 }
