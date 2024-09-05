65e21b00cc0c99039480ca345cc46fb4db75e67d
Jakub Schwenkbeck <Jakub@eSchwenkbeck>
2024-09-05 03:19:36 +0200

    new naming for distinction

--- include/password_cracker/brute_force.h
+++ include/password_cracker/brute_force.h
@@ -3,13 +3,10 @@
 
 #include <string>
 
-// Function to compute SHA256 hash of the input string
-std::string sha256(const std::string& str);
-
 // Recursive function to brute-force the password
 bool bruteForce(const std::string& current_guess, const std::string& target_hash, const std::string& characters, int max_length);
 
 // Main function to initiate brute-force cracking
-int Crack(const std::string& targetString, int maxLength);
+int brute_Crack(const std::string& targetString, int maxLength);
 
 #endif // BRUTE_FORCE__H
