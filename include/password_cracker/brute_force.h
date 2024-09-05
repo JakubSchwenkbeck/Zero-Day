0b142b9af39abad83ffb69705cab3c4bba667411
Jakub <168542356+JakubSchwenkbeck@users.noreply.github.com>
2024-09-05 03:18:10 +0200

    altered bruteforce header to match new naming

--- include/password_cracker/brute_force.h
+++ include/password_cracker/brute_force.h
@@ -7,6 +7,6 @@
 bool bruteForce(const std::string& current_guess, const std::string& target_hash, const std::string& characters, int max_length);
 
 // Main function to initiate brute-force cracking
-int Crack(const std::string& targetString, int maxLength);
+int brute_Crack(const std::string& targetString, int maxLength);
 
 #endif // BRUTE_FORCE__H
